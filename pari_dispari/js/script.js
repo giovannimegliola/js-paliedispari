426d2784dbf740e3036ddcb89a12ac4e225d02c0
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-17 17:36:10 +0200

    added playGame function

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -1,3 +1,23 @@
+
+
+const playBtn = document.getElementById('submitBtn');
+const result = document.getElementById('message');
+
+
+playBtn.addEventListener('click', playGame);
+
+
+function playGame() {
+const userChoice = document.getElementById('choice').value;
+const userNum = parseInt(document.getElementById('number').value);
+const computerNum = getRndInteger(1, 5);
+const somma = sumNumbers(userNum, computerNum);
+
+const gameResult = isEven(somma) ? 'pari' : 'dispari';
+
+message.innerText = (userChoice === gameResult) ? 'Hai vinto! Il numero del computer era ' + computerNum + ', la somma è ' + somma + ' (' + gameResult + ').' : 'Il computer ha vinto. Il numero del computer era ' + computerNum + ', la somma è ' + somma + ' (' + gameResult + ').';
+}
+
 function getRndInteger(min, max) {
 return Math.floor(Math.random() * (max - min + 1)) + min;
 }
