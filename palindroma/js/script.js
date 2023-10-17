9a9d135a24e4fbd64c0a8edf9f5445f8067ceb45
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-17 16:13:32 +0200

    added if condition to print an appropriate message

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -7,4 +7,14 @@ function isPalindrome(word) {
   return word === word.split('').reverse().join('');
 }
 
+document.getElementById('submitBtn').addEventListener('click', function() {   //acquisizione della parole inserita dall'utente
+  var inputWord = document.getElementById('word').value;
+  var result = document.getElementById('message');
+  
+  if (isPalindrome(inputWord)) {       //stampa dell'esito
+      result.innerText = 'La parola è palindroma!';
+  } else {
+      result.innerText = 'La parola non è palindroma.';
+  }
+});
 
