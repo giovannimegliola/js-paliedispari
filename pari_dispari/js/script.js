868d670212295451b45ed46c832674239df23c0f
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-17 16:45:01 +0200

    added utilities functions

---
+++ pari_dispari/js/script.js
@@ -0,0 +1,11 @@
+function getRndInteger(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sumNumbers(num1, num2) {
+return num1 + num2;
+}
+
+function isEven(num) {
+return num % 2 === 0;
+}
